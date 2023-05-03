6aa06a5b50a8a7aea2e7ac0f040b6382f965108f
12roshan12 <roshansharma7501@gmail.com>
2023-05-03 21:11:43 +0545

    ch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  Main_Api: 'https://tame-teal-centipede-slip.cyclic.app/api/'
+  // Main_Api: 'https://tame-teal-centipede-slip.cyclic.app/api/'
+  Main_Api: 'https://backend.elpasoadventure.com/api/'
 };
